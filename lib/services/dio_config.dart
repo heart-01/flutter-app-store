d351717d98be649de453a698d29bddf4db4c3d73
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-05 13:23:38 +0700

    [Fix]: Refactor DioConfig interceptors

--- lib/services/dio_config.dart
+++ lib/services/dio_config.dart
@@ -6,36 +6,42 @@ class DioConfig {
   static final Dio _dio = Dio()
 
     // .. is reference instance or object _dio == new Dio();
-    ..interceptors.add(InterceptorsWrapper(onRequest: (options, handler) async {
-      options.headers['Accept'] = 'application/json';
-      options.headers['Content-Type'] = 'application/json';
-      options.baseUrl = baseURLAPI;
-      return handler.next(options);
-    }, onResponse: (response, handler) async {
-      return handler.next(response); // continue
-    }, onError: (DioException e, handler) async {
-      switch (e.response?.statusCode) {
-        case 400:
-          logger.e('Bad Request');
-          break;
-        case 401:
-          logger.e('Unauthorized');
-          break;
-        case 403:
-          logger.e('Forbidden');
-          break;
-        case 404:
-          logger.e('Not Found');
-          break;
-        case 500:
-          logger.e('Internal Server Error');
-          break;
-        default:
-          logger.e('Something went wrong');
-          break;
-      }
-      return handler.next(e);
-    }));
+    ..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          options.headers['Accept'] = 'application/json';
+          options.headers['Content-Type'] = 'application/json';
+          options.baseUrl = baseURLAPI;
+          return handler.next(options);
+        },
+        onResponse: (response, handler) async {
+          return handler.next(response); // continue
+        },
+        onError: (DioException e, handler) async {
+          switch (e.response?.statusCode) {
+            case 400:
+              logger.e('Bad Request');
+              break;
+            case 401:
+              logger.e('Unauthorized');
+              break;
+            case 403:
+              logger.e('Forbidden');
+              break;
+            case 404:
+              logger.e('Not Found');
+              break;
+            case 500:
+              logger.e('Internal Server Error');
+              break;
+            default:
+              logger.e('Something went wrong');
+              break;
+          }
+          return handler.next(e);
+        },
+      ),
+    );
 
   // method name dio is getter return instance _dio
   static Dio get dio => _dio;
