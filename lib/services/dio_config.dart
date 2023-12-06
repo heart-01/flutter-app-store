8b24a52471740961f4e9d416a94ea1b5c4fe35f7
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-06 18:13:00 +0700

    [Feat]: Add authentication to Dio instance

--- lib/services/dio_config.dart
+++ lib/services/dio_config.dart
@@ -1,10 +1,18 @@
+import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter_store/utils/constants.dart';
 import 'package:flutter_store/utils/utility.dart';
 
 class DioConfig {
-  static final Dio _dio = Dio()
+  static late String accessToken;
 
+  static getToken() async {
+    final user = jsonDecode(await Utility.getSharedPreference('user'));
+    accessToken = user['token'].toString();
+  }
+
+  // Create Dio Instance
+  static final Dio _dio = Dio()
     // .. is reference instance or object _dio == new Dio();
     ..interceptors.add(
       InterceptorsWrapper(
@@ -43,6 +51,48 @@ class DioConfig {
       ),
     );
 
+  // Create Dio Instance with Auth
+  static final Dio _dioWithAuth = Dio()
+    ..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          await getToken();
+          options.headers['Authorization'] = 'Bearer $accessToken';
+          options.headers['Accept'] = 'application/json';
+          options.headers['Content-Type'] = 'application/json';
+          options.baseUrl = baseURLAPI;
+          return handler.next(options);
+        },
+        onResponse: (response, handler) async {
+          return handler.next(response);
+        },
+        onError: (DioException e, handler) {
+          switch (e.response?.statusCode) {
+            case 400:
+              Utility().logger.e('Bad Request');
+              break;
+            case 401:
+              Utility().logger.e('Unauthorized');
+              break;
+            case 403:
+              Utility().logger.e('Forbidden');
+              break;
+            case 404:
+              Utility().logger.e('Not Found');
+              break;
+            case 500:
+              Utility().logger.e('Internal Server Error');
+              break;
+            default:
+              Utility().logger.e('Something went wrong');
+              break;
+          }
+          return handler.next(e);
+        },
+      ),
+    );
+
   // method name dio is getter return instance _dio
   static Dio get dio => _dio;
+  static Dio get dioWithAuth => _dioWithAuth;
 }
