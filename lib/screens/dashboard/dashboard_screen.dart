b881619072b932bc0dfdfedbce26796fb83e21db
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-06 16:50:39 +0700

    [Feat]: Add logout functionality and drawer menu to DashboardScreen

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,13 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_store/app_router.dart';
+import 'package:flutter_store/utils/utility.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  _logout() {
+    // Remove user shared preference
+    Utility.removeSharedPreference('user');
+
+    // Clear all route and push to login screen
+    Navigator.pushNamedAndRemoveUntil(
+      context,
+      AppRouter.login,
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dashboard'),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: Column(
+          children: [
+            ListView(
+              shrinkWrap: true,
+              children: [
+                const UserAccountsDrawerHeader(
+                  accountName: Text('Siwat Jermwatthana'),
+                  accountEmail: Text('admin@email.com'),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/avatar.jpeg'),
+                  ),
+                  otherAccountsPictures: [
+                    CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/images/noavartar.png'),
+                    ),
+                  ],
+                ),
+                ListTile(
+                  leading: const Icon(Icons.info_outline),
+                  title: const Text('Info'),
+                  onTap: () {
+                    Navigator.pushNamed(context, AppRouter.info);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.person_outline),
+                  title: const Text('About'),
+                  onTap: () {
+                    Navigator.pushNamed(context, AppRouter.about);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.email_outlined),
+                  title: const Text('Contact'),
+                  onTap: () {
+                    Navigator.pushNamed(context, AppRouter.contact);
+                  },
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.exit_to_app_outlined),
+                    title: const Text('Logout'),
+                    onTap: _logout,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       body: const Center(
         child: Text('Dashboard Screen'),
