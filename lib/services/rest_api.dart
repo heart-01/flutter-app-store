6e7a2c5fe2e8490f329994255d48a8b90c14628e
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-06 18:13:27 +0700

    [Feat]: Add getAllProducts API

--- lib/services/rest_api.dart
+++ lib/services/rest_api.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
+import 'package:flutter_store/models/product_model.dart';
 import 'package:flutter_store/services/dio_config.dart';
 import 'package:flutter_store/utils/utility.dart';
 
 class CallAPI {
   // Dio Instance
   final Dio _dio = DioConfig.dio;
+  final Dio _dioWithAuth = DioConfig.dioWithAuth;
 
   // Register API
   registerAPI(data) async {
@@ -37,5 +39,18 @@ class CallAPI {
         Utility().logger.e(error);
       }
     }
+  }
+
+  // Get All Product API
+  Future<List<ProductModel>> getAllProducts() async {
+    final response = await _dioWithAuth.get('products');
+    if (response.statusCode == 200) {
+      Utility().logger.d(response.data);
+      final List<ProductModel> products = productModelFromJson(
+        json.encode(response.data),
+      );
+      return products;
+    }
+    throw Exception('Failed to load products');
   }
 }
