0ed105593a6bc95c8a70bc9b27094187d90b2339
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-07 21:25:22 +0700

    [Fix]: Update font sizes and theme colors

--- lib/themes/styles.dart
+++ lib/themes/styles.dart
@@ -2,19 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_store/themes/colors.dart';
 
 class AppTheme {
+
   // Custom text theme
   static const customTextTheme = TextTheme(
     bodyMedium: TextStyle(
       fontSize: 16,
     ),
     bodyLarge: TextStyle(
-      fontSize: 24,
+      fontSize: 18,
     ),
   );
 
   // Light theme
   static final ThemeData lightTheme = ThemeData(
-    useMaterial3: false,
+    useMaterial3: true,
     fontFamily: 'NotoSansThai',
     primaryColor: primary,
     primaryColorDark: primaryDark,
@@ -24,11 +25,15 @@ class AppTheme {
     colorScheme: const ColorScheme.light(primary: primary),
     iconTheme: const IconThemeData(color: primaryText),
     scaffoldBackgroundColor: Colors.blueGrey[100],
+    drawerTheme: const DrawerThemeData(
+      backgroundColor: primary,
+    ),
     appBarTheme: const AppBarTheme(
       titleTextStyle: TextStyle(
+        color: icons,
         fontFamily: 'NotoSansThai',
         fontWeight: FontWeight.bold,
-        fontSize: 20,
+        fontSize: 18,
       ),
       backgroundColor: primary,
       foregroundColor: icons,
@@ -38,7 +43,7 @@ class AppTheme {
 
   // dark theme
   static final ThemeData darkTheme = ThemeData(
-    useMaterial3: false,
+    useMaterial3: true,
     fontFamily: 'NotoSansThai',
     primaryColor: primary,
     primaryColorDark: primaryDark,
@@ -48,15 +53,21 @@ class AppTheme {
     colorScheme: const ColorScheme.dark(primary: icons),
     iconTheme: const IconThemeData(color: icons),
     scaffoldBackgroundColor: primaryText,
+    navigationDrawerTheme: const NavigationDrawerThemeData(
+      backgroundColor: primaryText,
+      elevation: 0,
+    ),
     appBarTheme: const AppBarTheme(
       titleTextStyle: TextStyle(
+        color: icons,
         fontFamily: 'NotoSansThai',
         fontWeight: FontWeight.bold,
-        fontSize: 20,
+        fontSize: 18,
       ),
       backgroundColor: primaryText,
       foregroundColor: icons,
       iconTheme: IconThemeData(color: icons),
     ),
   );
+
 }
